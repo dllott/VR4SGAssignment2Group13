a92bfadacdca3ccfb986d475d3d1a9922c81cf4a
JoshSwerdlow <jswerds18@ufl.edu>
2019-03-17 19:07:06 -0400

    sprint 1

---
+++ Assets/Scripts/InsulinInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class InsulinInfo : MonoBehaviour
+{
+    public Text displayInsulin;
+    public GameObject insulin;
+    public int doses = 28;
+    string sDoses;
+    // Start is called before the first frame update
+    void Start()
+    {
+        insulin = GameObject.Find("Insulin");
+        displayInsulin = insulin.GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sDoses = doses.ToString();
+        displayInsulin.text = "Doses left: " + sDoses;
+    }
+    void medicated()
+    {
+        if (doses != 0)
+        {
+            doses -= 1;
+        }
+    }
+}
