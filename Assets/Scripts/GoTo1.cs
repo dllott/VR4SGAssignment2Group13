955cc1374da8a56f8789bb01b20fc2e08f68fac5
SeanCStanley <seanchristopherstanley@gmail.com>
2019-03-18 08:58:57 -0400

    Made basic intro and exit scenes with default text, added transition from intro to main

---
+++ Assets/Scripts/GoTo1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//based on https://answers.unity.com/questions/796881/c-how-can-i-let-something-happen-after-a-small-del.html
+public class GoTo1 : MonoBehaviour {
+
+    void Transition()
+    {
+        Debug.Log("Scene Transition time");
+        SceneManager.LoadScene("Assets/Main");
+    }
+    // Use this for initialization
+    void Start () {
+        Invoke("Transition", 15);
+	}
+
+}
