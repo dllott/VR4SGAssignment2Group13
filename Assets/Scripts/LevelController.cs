96e9b19799062f85795d390e2bc3c81141761d57
D. Lott <dllott99@ufl.edu>
2019-03-31 23:39:44 -0400

    linked everything i hope

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -50,6 +50,32 @@ public class LevelController : MonoBehaviour
         darken.fillAmount = 1;
     }
 
+    void OnEnable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled.
+        //Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Level Loaded");
+        Debug.Log(scene.name);
+        Debug.Log(mode);
+        GameObject player = GameObject.Find("Player");
+        darken = GameObject.Find("DarkenImage").GetComponent<Image>();
+        introduction = GameObject.Find("introtext").GetComponent<Text>();
+        GameObject camera = player.transform.Find("Main Camera").gameObject;
+        blurScript = camera.GetComponent<BlurOptimized>();
+        
+    }
+
     void Update()
     {
         if (c.a > 0f)
