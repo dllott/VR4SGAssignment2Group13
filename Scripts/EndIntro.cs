cfcda97222f4fac821ab77945cca9b559687ed17
wlin1997 <wlin1997@gmail.com>
2019-04-14 20:50:55 -0400

    Add files via upload

---
+++ Scripts/EndIntro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndIntro : MonoBehaviour
+{
+
+    void Transition()
+    {
+        Debug.Log("Intro Ended");
+        SceneManager.LoadScene("Scenes/Main");
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("Transition", 7);
+    }
+
+}
---
+++ Scripts/GameClock.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+//static var hour = 6;
+
+public class GameClock : MonoBehaviour
+{
+    //public int day = 0;
+    public int maxDay = 28;
+    private Text displayClock;
+    public Health hunger;
+    //public int hour = 7;
+    //static int minutes = 0;
+    //static string dayNight = "A.M.";
+    double secondsSinceStart = 0.0;
+    public bool bedTime = false;
+    LevelController levelController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelController = GameObject.Find("LevelController").GetComponent<LevelController>();
+        hunger = GameObject.Find("fridge").GetComponent<Health>();
+        displayClock = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Time.time - secondsSinceStart > 1.0)
+        {
+            minutes++;
+            if(minutes == 60)
+            {
+                minutes = 0;
+                hour++;
+                if(hour >= 12)
+                {
+                    if (dayNight == "A.M.")
+                    {
+                        dayNight = "P.M.";
+                    }
+                    else
+                    {
+                        //insul.SetNeed(true);
+                        hour = 7;
+                        dayNight = "A.M.";
+                    }
+                }
+                if(hour >= 13)
+                {
+                    hour -= 12;
+                }
+
+            }
+            secondsSinceStart = Time.time;
+        }*/
+        string dispHour = LeadingZero(levelController.hour);
+        string dispMinutes = LeadingZero(levelController.minutes);
+        string dispDay = LeadingZero(levelController.Day);
+        displayClock.text = dispHour + ":" + dispMinutes + levelController.dayNight + "    Day: " + dispDay;
+        if(levelController.dayNight == "P.M." && levelController.hour > 7 && levelController.hour != 12 || levelController.dayNight == "A.M." && levelController.hour < 6)
+        {
+            bedTime = true;
+        } else
+        {
+            bedTime = false;
+        }
+    }
+    public void working()
+    {
+        hunger.CountHunger(8);
+        levelController.hour += 4;
+        CheckDayNight();
+    }
+    public void sleeping()
+    {
+        hunger.CountHunger(10);
+        levelController.hour += 8;
+        CheckDayNight();
+    }
+    public void MoveTime(int t)
+    {
+        levelController.hour += t;
+        CheckDayNight();
+    }
+    string LeadingZero(int x)
+    {
+        return x.ToString().PadLeft(2, '0');
+    }
+
+    void CheckDayNight()
+    {
+        if (levelController.hour > 12)
+        {
+            if (levelController.dayNight == "A.M.")
+            {
+                levelController.dayNight = "P.M.";
+            }
+            else
+            {
+                levelController.hour = 7;
+                levelController.IncrementDay();
+                levelController.dayNight = "A.M.";
+            }
+        }
+        if (levelController.hour >= 13)
+        {
+            levelController.hour -= 12;
+        }
+    }
+}
